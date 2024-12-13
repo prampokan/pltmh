865eb0681834f87009d8e78394df54d7f68b90ec
prampokan <prampokan@gmail.com>
2024-12-13 09:09:18 +0700

    initial commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,32 @@
+import { SidebarTrigger } from "./ui/sidebar";
+import { Separator } from "./ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full h-14 flex items-center px-2 border-b fixed bg-white">
+      <div className="flex items-center gap-x-2">
+        <SidebarTrigger />
+        <Separator orientation="vertical" className="mr-2 h-4" />
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="hidden md:block">
+              <BreadcrumbLink href="#">Dashboard</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator className="hidden md:block" />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Dashboard</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+    </nav>
+  );
+}
