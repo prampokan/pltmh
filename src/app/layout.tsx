2928bb3d8f57575822066cbeb856b5f7b9037671
prampokan <prampokan@gmail.com>
2024-12-13 14:55:55 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import Navbar from "@/components/navbar";
+import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
@@ -15,7 +13,12 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function DashboardLayout({
+export const metadata: Metadata = {
+  title: "PLTMH Kincang",
+  description: "Sistem Monitoring PLTMH Kincang",
+};
+
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -31,13 +34,7 @@ export default function DashboardLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-              <Navbar />
-              <div className="py-20 px-5">{children}</div>
-            </SidebarInset>
-          </SidebarProvider>
+          {children}
         </ThemeProvider>
       </body>
     </html>
