101f2f4ce5513bb6693a930f9dfbabffdfb3747b
prampokan <prampokan@gmail.com>
2025-02-25 02:05:16 +0700

    update

--- src/app/(dashboard)/realtime-sensors/page.tsx
+++ src/app/(dashboard)/realtime-sensors/page.tsx
@@ -1,15 +1,155 @@
+"use client";
+
 import Header from "@/components/header";
-import Kelistrikan from "../components/kelistrikan";
+import RealtimeCard from "@/components/realtime-card";
+import { LatestTelemetry } from "@/service";
 
 export default function RealtimeSensors() {
+  const dataHidrometri = LatestTelemetry(
+    "50826780-ec35-11ef-9389-77a321a8daf2",
+    20
+  );
+  const dataPowermeter = LatestTelemetry(
+    "e2d1b230-ec35-11ef-9389-77a321a8daf2",
+    10
+  );
   return (
     <main>
       <Header
         head="Realtime Sensors"
         body="Selamat datang di Website Sistem Monitoring dan Prediksi PLTMH Kincang, Banjarnegara. Selamat Bekerja!"
       />
-      <div className="grid grid-cols-5 gap-4">
-        <Kelistrikan />
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
+        {dataHidrometri &&
+          dataHidrometri.waterLevel &&
+          dataHidrometri.waterLevel[0] &&
+          dataHidrometri.waterLevel[0][1] && (
+            <RealtimeCard
+              title="Water Level"
+              value={dataHidrometri.waterLevel[0][1] / 1000}
+              unit="m"
+            />
+          )}
+        {dataHidrometri &&
+          dataHidrometri.VelocityofFlow &&
+          dataHidrometri.VelocityofFlow[0] &&
+          dataHidrometri.VelocityofFlow[0][1] && (
+            <RealtimeCard
+              title="Velocity of Flow"
+              value={dataHidrometri.VelocityofFlow[0][1] / 1000}
+              unit="m/s"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value12 &&
+          dataPowermeter.Value12[0] &&
+          dataPowermeter.Value12[0][1] && (
+            <RealtimeCard
+              title="Active Power"
+              value={dataPowermeter.Value12[0][1] / 10}
+              unit="kW"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value14 &&
+          dataPowermeter.Value14[0] &&
+          dataPowermeter.Value14[0][1] && (
+            <RealtimeCard
+              title="Power Factor"
+              value={dataPowermeter.Value14[0][1] / 1000}
+              unit="PF"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value28 &&
+          dataPowermeter.Value28[0] &&
+          dataPowermeter.Value28[0][1] && (
+            <RealtimeCard
+              title="Reactive Power"
+              value={dataPowermeter.Value28[0][1] / 1000}
+              unit="kVAR"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value15 &&
+          dataPowermeter.Value15[0] &&
+          dataPowermeter.Value15[0][1] && (
+            <RealtimeCard
+              title="Frequency"
+              value={dataPowermeter.Value12[0][1] / 100}
+              unit="Hz"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value20 &&
+          dataPowermeter.Value20[0] &&
+          dataPowermeter.Value20[0][1] && (
+            <RealtimeCard
+              title="Speed Turbin"
+              value={dataPowermeter.Value20[0][1] / 10}
+              unit="RPM"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value21 &&
+          dataPowermeter.Value21[0] &&
+          dataPowermeter.Value21[0][1] && (
+            <RealtimeCard
+              title="Voltage Phase R"
+              value={dataPowermeter.Value21[0][1] / 1000}
+              unit="V"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value22 &&
+          dataPowermeter.Value22[0] &&
+          dataPowermeter.Value22[0][1] && (
+            <RealtimeCard
+              title="Voltage Phase S"
+              value={dataPowermeter.Value22[0][1] / 1000}
+              unit="V"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value23 &&
+          dataPowermeter.Value23[0] &&
+          dataPowermeter.Value23[0][1] && (
+            <RealtimeCard
+              title="Voltage Phase T"
+              value={dataPowermeter.Value21[0][1] / 1000}
+              unit="V"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value24 &&
+          dataPowermeter.Value24[0] &&
+          dataPowermeter.Value24[0][1] && (
+            <RealtimeCard
+              title="Current Phase R"
+              value={dataPowermeter.Value24[0][1] / 1000}
+              unit="A"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value25 &&
+          dataPowermeter.Value25[0] &&
+          dataPowermeter.Value25[0][1] && (
+            <RealtimeCard
+              title="Current Phase S"
+              value={dataPowermeter.Value25[0][1] / 1000}
+              unit="A"
+            />
+          )}
+        {dataPowermeter &&
+          dataPowermeter.Value26 &&
+          dataPowermeter.Value26[0] &&
+          dataPowermeter.Value26[0][1] && (
+            <RealtimeCard
+              title="Current Phase T"
+              value={dataPowermeter.Value26[0][1] / 1000}
+              unit="A"
+            />
+          )}
       </div>
     </main>
   );
---
+++ src/components/realtime-card.tsx
@@ -0,0 +1,17 @@
+export default function RealtimeCard({ title, description, value, unit }: any) {
+  return (
+    <div className="w-full border rounded-lg">
+      <div className="border-b p-4">
+        <h1 className="font-bold">{title}</h1>
+        <p className="text-sm text-muted-foreground">
+          Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+      <div className="p-4 flex items-center justify-center h-44">
+        <h1 className="font-bold text-xl">
+          {value} <span className="text-sm font-light">{unit}</span>
+        </h1>
+      </div>
+    </div>
+  );
+}
